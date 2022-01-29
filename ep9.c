afdd8217140a3dd98ac6807d49b2164eccee150e
himanshubawankar <98639879+himanshubawankar@users.noreply.github.com>
2022-01-29 14:31:20 +0530

    Add files via upload

---
+++ ep9.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#define PI 3.14
+int main()
+{
+    /*
+        My name is himanshu
+    */
+    int a = 8;
+    float b = 7.333;
+    printf("tab character \t my backslash \\n  %f",PI);    
+    // printf("The value of a is %d and value of b is %f\n",a , b);
+    return 0;
+}
